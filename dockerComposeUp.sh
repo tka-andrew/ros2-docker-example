00bf338d927b57c75d345dee18c40eee5e7fb372
tka-andrew <kui.an.andrew.tan@gmail.com>
2022-05-07 17:56:06 +0800

    added necessary lines for creating xauth file

--- dockerComposeUp.sh
+++ dockerComposeUp.sh
@@ -1,3 +1,9 @@
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+# REFERENCE: https://stackoverflow.com/questions/71245228/running-x-server-using-docker
+# This is neede to create the xauth file
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
 # REFERENCE: https://stackoverflow.com/questions/53106678/docker-compose-up-and-user-inputs-on-stdin
 # This is not enough to use the turtle_teleop, we need to attach to the turtle_teleop service in another terminal
 sudo docker-compose up --remove-orphan
