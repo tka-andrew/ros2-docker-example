9b7abe218a7c9b3c90f2adbe3c834a3f92f35342
tka-andrew <kui.an.andrew.tan@gmail.com>
2022-05-07 17:57:51 +0800

    minor typo fix

--- dockerComposeUp.sh
+++ dockerComposeUp.sh
@@ -1,7 +1,7 @@
 XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker.xauth
 # REFERENCE: https://stackoverflow.com/questions/71245228/running-x-server-using-docker
-# This is neede to create the xauth file
+# This is needed to create the xauth file
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
 # REFERENCE: https://stackoverflow.com/questions/53106678/docker-compose-up-and-user-inputs-on-stdin
